ff5e388ca953494674a475301f8caa1b4a0d4ede
Roman Bielokon <roman_slavyansk@mail.ru>
2014-03-16 20:05:02 +0200

    tests

--- src/com/kvest/parse_test/ui/activity/MyActivity.java
+++ src/com/kvest/parse_test/ui/activity/MyActivity.java
@@ -2,10 +2,22 @@ package com.kvest.parse_test.ui.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 import com.kvest.parse_test.R;
+import com.parse.FindCallback;
+import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import java.util.List;
 
 public class MyActivity extends Activity {
+    private static final String TAG = "KVEST_TAG";
+    private EditText name;
+
     /**
      * Called when the activity is first created.
      */
@@ -14,10 +26,40 @@ public class MyActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        ParseObject user = new ParseObject("User");
-        user.put("user_id", System.currentTimeMillis());
-        user.put("name", "test");
-        user.put("gender", 1);
-        user.saveInBackground();
+        name = (EditText)findViewById(R.id.name);
+        findViewById(R.id.add).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nameValue = name.getText().toString();
+                if (!TextUtils.isEmpty(nameValue)) {
+                    ParseObject user = new ParseObject("User");
+                    user.put("user_id", System.currentTimeMillis());
+                    user.put("name", nameValue);
+                    user.put("gender", 1);
+                    user.saveInBackground();
+                }
+            }
+        });
+
+        findViewById(R.id.get_all).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ParseQuery<ParseObject> query = ParseQuery.getQuery("User");
+                query.orderByDescending("user_id");
+                query.findInBackground(new FindCallback<ParseObject>() {
+                    @Override
+                    public void done(List<ParseObject> parseObjects, ParseException e) {
+                        if (e == null) {
+                            Log.d(TAG, "Retrieved " + parseObjects.size() + " scores");
+                            for (ParseObject object : parseObjects) {
+                                Log.d(TAG, object.get("user_id") + ": " + object.get("name"));
+                            }
+                        } else {
+                            Log.d(TAG, "Error: " + e.getMessage());
+                        }
+                    }
+                });
+            }
+        });
     }
 }
